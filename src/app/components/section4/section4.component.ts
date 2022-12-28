7fc4cb700f72c9775116afadca779542fc4aa18b
葉濬宇 Thomas <thomas@miniasp.com>
2022-12-28 22:45:46 +0800

    完成進度: 1. section4 動畫完成

--- src/app/components/section4/section4.component.ts
+++ src/app/components/section4/section4.component.ts
@@ -1,4 +1,11 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 @Component({
@@ -8,6 +15,9 @@ import ScrollTrigger from 'gsap/ScrollTrigger';
 })
 export class Section4Component implements OnInit {
   @ViewChild('ul') ul!: ElementRef;
+  @ViewChild('tank') tank!: ElementRef;
+  @ViewChildren('week') weeks!: QueryList<ElementRef>;
+  @ViewChildren('flag') flags!: QueryList<ElementRef>;
   constructor() {}
 
   ngOnInit(): void {
@@ -20,19 +30,66 @@ export class Section4Component implements OnInit {
 
   horizontalAnimate() {
     let ul = this.ul.nativeElement;
-    let liItems = ul.children;
+    let weeks = this.weeks.map((week) => week.nativeElement);
+    let flags = this.flags.map((flag) => flag.nativeElement);
+    let oneWeekWidth = weeks[0].offsetWidth;
 
-    gsap.to(liItems, {
-      ease: 'none',
-      xPercent: -100 * (liItems.length - 1),
+    let timeline = gsap.timeline({
       scrollTrigger: {
         trigger: ul,
         pin: true,
         start: 'top top',
         scrub: true,
-        snap: 1 / (liItems.length - 1),
-        end: () => '+=' + ul.offsetWidth,
+        end: '+=' + oneWeekWidth * 2,
       },
     });
+    timeline.to(weeks, {
+      ease: 'none',
+      xPercent: -100 * (weeks.length - 1),
+    });
+
+    flags.forEach((flag: HTMLImageElement,index:number) => {
+      gsap.to(flag, {
+        scrollTrigger: {
+          trigger: flag,
+          start: ()=>{
+            let flagStart:number|string;
+            if(index == 0){
+              flagStart = "top"
+            }
+            else if(index == 1){
+              flagStart = oneWeekWidth*0.5
+            }
+            else{
+              flagStart = oneWeekWidth*1.5
+            }
+            return `+=${flagStart}px center`
+          },
+          end: ()=>{
+            let flagEnd:number|string;
+            if(index == 0){
+              flagEnd= "bottom"
+            }else{
+              flagEnd = index * oneWeekWidth / 2
+            }
+            return `+=${flagEnd}px top`
+          },
+          scrub: true,
+          onUpdate(timeline) {
+            let progress = timeline.progress;
+            if (progress < 0.1) {
+              flag.src = 'assets/img/flag/F1.png';
+            } else if (progress >= 0.1 && progress < 0.3) {
+              flag.src = 'assets/img/flag/F2.png';
+            } else if (progress >= 0.3 && progress < 0.6) {
+              flag.src = 'assets/img/flag/F3.png';
+            }else{
+              flag.src = 'assets/img/flag/F4.png';
+            }
+          },
+        },
+      });
+    });
   }
+
 }
