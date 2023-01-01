76405f1229489746d7858ed0506b43c2f31b3226
葉濬宇 Thomas <thomas@miniasp.com>
2023-01-01 17:36:03 +0800

    完成進度: 1. section 6、7、8、9版型

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,10 @@ import { Section2Component } from './components/section2/section2.component';
 import { Section3Component } from './components/section3/section3.component';
 import { Section4Component } from './components/section4/section4.component';
 import { Section5Component } from './components/section5/section5.component';
+import { Section6Component } from './components/section6/section6.component';
+import { Section7Component } from './components/section7/section7.component';
+import { Section8Component } from './components/section8/section8.component';
+import { Section9Component } from './components/section9/section9.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +28,10 @@ import { Section5Component } from './components/section5/section5.component';
     Section3Component,
     Section4Component,
     Section5Component,
+    Section6Component,
+    Section7Component,
+    Section8Component,
+    Section9Component,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
---
+++ src/app/components/section7/section7.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-section-7',
+  templateUrl: './section7.component.html',
+  styleUrls: ['./section7.component.scss']
+})
+export class Section7Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
