6e94f7999e49804fa8f4fdfff34c8d52b3bcfd86
葉濬宇 Thomas <thomas@miniasp.com>
2023-01-03 22:14:44 +0800

    今日進度 1. section9新增動畫 2. section8調整rwd 3. section6新增動畫

--- src/app/components/section6/section6.component.ts
+++ src/app/components/section6/section6.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
 @Component({
   selector: 'app-section-6',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./section6.component.scss']
 })
 export class Section6Component implements OnInit {
+  @ViewChild("text") text!:ElementRef;
+  @ViewChild("contain") contain!:ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
+    gsap.registerPlugin(ScrollTrigger);
+  }
+
+  ngAfterViewInit(){
+    let text = this.text.nativeElement;
+    let contain = this.contain.nativeElement;
+
+    gsap.from(text,{
+      perspective:"500px",
+      transformStyle:"preserve-3d",
+      rotateX:"90deg",
+      scale:0.5,
+      duration:0.5,
+      scrollTrigger:{
+        trigger:contain
+      }
+    })
   }
 
 }
